c51196875a0863f2fb3279814ada9177ba3875de
mapascua <StainlessDroid@gmail.com>
2025-11-02 20:31:00 +0100

    moved mason config to plugins / fix anoying nvim-tree behaviour

--- init.lua
+++ init.lua
@@ -14,27 +14,31 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("let g:user42='mapascua'")
 vim.cmd("let g:mail42='mapascua@student.42malaga.com'")
 
--- Open Tree-sitter (nvim-tree) in current dir when no args
+-- Open nvim-tree on startup
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if vim.fn.argc() == 0 then
-      require("nvim-tree.api").tree.open()
-    end
-  end,
+	callback = function(data)
+		if vim.fn.argc() == 0 or (vim.fn.argc() > 0 and vim.fn.isdirectory(data.file) == 1) then
+			return
+		end
+    	require("nvim-tree.api").tree.open()
+    	require("nvim-tree.api").tree.focus()
+    	if vim.fn.argc() > 0 then
+			vim.cmd("wincmd p") -- return to file window
+		end
+	end,
+})
+-- Close when nvim-tree is the only thing left
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function(data)
+	if vim.fn.isdirectory(data.file) ~= 1 then
+		if (vim.fn.winnr('$') == 1 and vim.bo.filetype == 'NvimTree') then
+				vim.cmd('quit')
+			end
+		end
+	end,
 })
 
--- LSP config ---
-require('mason').setup()
-require('mason-lspconfig').setup()
-require('mason-tool-installer').setup({
-	ensure_installed = {
-		"lua_ls",
-		"stylua",
-		"pyright",
-		"clangd"
-	}
-})
-
+-- LSP config --
 vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
